1087c67f7775dc6647d267af4943020efcccfb32
but3k4 <cbsfilho@gmail.com>
2011-03-10 16:36:25 -0300

    Modified modules and deleted .pyc files

--- modules/database.py
+++ modules/database.py
@@ -7,7 +7,6 @@ Copyright 2008, Claudio Borges
 Licensed under BSD License.
 """
 import sqlite3 as sqlite
-import re
 from types import *
 
 class Database(object):
@@ -17,52 +16,37 @@ class Database(object):
         self.cursor = self.conn.cursor()
 
     def add(self, table, content):
-        content = content.strip()
-        invalid = [ 'select', 'drop', 'insert', '[\`\']' ]
-        injection = None
-        for value in invalid:
-            if re.compile(value, re.I).search(content):
-                injection = 1
-                break
-
-        if not injection:
-            sql = "INSERT INTO %s VALUES (NULL, '%s')" % (table, content)
+        try:
+            sql = "INSERT INTO %s VALUES (NULL, '%s')" % (table, content.strip())
             self.cursor.execute(sql)
             self.conn.commit()
-        else:
+        except:
             return -1
 
     def add_gtalk(self, content1, content2):
-        content1 = content1.strip()
-        content2 = content2.strip()
-        invalid = [ 'select', 'drop', 'insert', '[\`\']' ]
-        injection = None
-        for value in invalid:
-            if re.compile(value, re.I).search(content1) or re.compile(value, re.I).search(content2):
-                injection = 1
-                break
-
-        if not injection:
-            sql = "INSERT INTO gtalk VALUES ('%s', '%s')" % (content1, content2)
+        try:
+            sql = "INSERT INTO gtalk VALUES ('%s', '%s')" % (content1.strip(), content2.strip())
             self.cursor.execute(sql)
             self.conn.commit()
-        else:
+        except:
             return -1
 
     def get_random(self, field, table):
-        self.cursor.execute("SELECT %s FROM %s ORDER BY random() LIMIT 1" % (field, table))
-	try:
-        	result = self.cursor.fetchone()[0]
-	except:
-		result = "Error"
-        return result
+        try:
+            self.cursor.execute("SELECT %s FROM %s ORDER BY random() LIMIT 1" % (field, table))
+            return self.cursor.fetchone()[0]
+        except:
+            return -1
 
     def get_value(self, field1, table, field2, nick=None):
         if nick != None:
-            self.cursor.execute("SELECT %s FROM %s WHERE %s = '%s'" % (field1, table, field2, nick))
-            result = self.cursor.fetchone()
-            if type(result) is not NoneType:
-                return result[0]
+            try:
+                self.cursor.execute("SELECT %s FROM %s WHERE %s = '%s'" % (field1, table, field2, nick))
+                result = self.cursor.fetchone()
+                if type(result) is not NoneType:
+                    return result[0]
+            except:
+                return -1
 
     def disconect(self):
         self.cursor.close()
--- modules/logger.py
+++ modules/logger.py
@@ -6,8 +6,9 @@ logger.py - lokky logger module
 Copyright 2008, Claudio Borges
 Licensed under BSD License.
 """
-import time
-import os
+from time import strftime
+from os.path import exists, join
+from os import makedirs
 from sys import stdout
 
 class Logger(object):
@@ -15,18 +16,15 @@ class Logger(object):
         self.debug = 0
 
     def __call__(self, string):
-        base = "log"
-        logdir = os.path.join(base, time.strftime("%Y%m"))
-        filename = os.path.join(logdir, time.strftime("%Y%m%d") + '.log')
-        string = '[' + time.strftime("%d/%m/%Y %H:%M:%S") + ']' + ' ' + string + '\n'
-        if os.path.exists(base) == False:
-            os.mkdir(base, 0755)
-        if os.path.exists(logdir) == False:
-            os.mkdir(logdir, 0755)
+        logdir = join('log', strftime("%Y"), strftime("%m"))
+        filename = "%s.log" % (join(logdir, strftime("%Y%m%d")))
+        string = "[ %s ] %s" % (strftime("%d/%m/%Y %H:%M:%S"), string)
+        if not exists(logdir):
+            makedirs(logdir, 0755)
         file = open(filename, 'a')
-        file.write(string)
+        file.write("%s\n" % string)
         if self.debug:
-            stdout.write(string)
+            stdout.write("%s\n" % string)
         file.close()
 
     def set_debug(self, debug=0):
