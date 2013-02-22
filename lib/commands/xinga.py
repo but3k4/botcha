e3f7296c289c51ace49dd4134ae17b634a126954
but3k4 <cbsfilho@gmail.com>
2013-02-21 23:10:19 -0300

    Fixing xinga command

--- lib/commands/xinga.py
+++ lib/commands/xinga.py
@@ -5,18 +5,18 @@ from lib.modules.database import Database
 
 class Xinga(Base_Command.Base_Command):
 
-    def xinga(self, nick):
-	if len(self.args) < 1:
-		self.parent.conn.privmsg(self.channel,'%s, deixa de ser burro e xinga alguém. ' % self.nick)	
-	else:
-	        db = Database()
-	        self.xinga = None
-	        try:
-	        	self.xinga = db.select('xinga', 'xingamentos', 1)[0][0]
-	        except:
-	            	return False
-	        if self.xinga:
-	            	self.parent.conn.privmsg(self.channel,'%s, %s' % (self.args[0],self.xinga))
+    def xinga(self):
+        if len(self.args) < 1:
+            self.parent.conn.privmsg(self.channel,'%s, deixa de ser burro e xinga alguém. ' % self.nick)	
+        else:
+            db = Database()
+            self.xinga = None
+            try:
+                self.xinga = db.select('xinga', 'xingamentos', 1)[0][0]
+            except:
+                return False
+            if self.xinga:
+                self.parent.conn.privmsg(self.channel, '%s, %s' % (self.args[0], self.xinga))
 
     def run(self):
-        self.xinga(self.nick)
+        self.xinga()
