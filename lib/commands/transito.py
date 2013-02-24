c63bbf123befce7f6d6ade1ee0125b605d63d4e4
but3k4 <cbsfilho@gmail.com>
2013-02-24 13:05:13 -0300

    Renamed search.py to web.py

--- lib/commands/transito.py
+++ lib/commands/transito.py
@@ -1,14 +1,14 @@
 #  -*- coding: utf-8 -*-
 #
 from lib.commands import Base_Command
-from lib.modules.search import Search
+from lib.modules.web import web
 
 class Transito(Base_Command.Base_Command):
 
     def transito(self):
-        search = Search()
+        search = Web()
         try:
-            self.parent.conn.privmsg(self.channel, search.cet())
+            self.parent.conn.privmsg(self.channel, web.cet())
         except:
             return False
 
--- lib/modules/web.py
+++ lib/modules/web.py
@@ -1,19 +1,19 @@
 #  -*- coding: utf-8 -*-
 #
 """
-search.py - lokky search module
+web.py - lokky web module
 Copyright 2008, Claudio Borges
 Licensed under BSD License.
 """
 import httplib2
 import BeautifulSoup
 
-class Search(object):
+class Web(object):
 
     def __init__(self):
         pass
 
-    def get_html(self, url):
+    def html(self, url):
         sock = httplib2.Http(timeout=5)
         headers, response = sock.request(url)
         if headers['status'] in (200, '200'):
@@ -21,7 +21,7 @@ class Search(object):
     
     def cet(self):
         try:
-            answer_data = self.get_html('http://cetsp1.cetsp.com.br/monitransmapa/agora/')
+            answer_data = self.html('http://cetsp1.cetsp.com.br/monitransmapa/agora/')
         except:
             return False
         dados = {
