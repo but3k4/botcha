dc832aaa3bf2d7943820be25ed1ff0a031d1fb7e
but3k4 <cbsfilho@gmail.com>
2013-03-09 20:47:41 -0300

    Removed twitterz.py module and fixed sl command

--- lib/commands/sl.py
+++ lib/commands/sl.py
@@ -1,14 +1,22 @@
 #  -*- coding: utf-8 -*-
 #
 from lib.commands import Base_Command
-from lib.modules.twitterz import Twitterz
+from lib.modules.web import Web
+import random
+import re
 
 class Sl(Base_Command.Base_Command):
 
     def sl(self):
-        tw = Twitterz()
+        user = 'iLikeGirlsDaily'
+        number = 60
+        web = Web()
         try:
-            self.parent.conn.privmsg(self.channel, tw.ilikegirlz())
+            answer = web.json(web.get("http://search.twitter.com/search.json?q=@%s&rpp=%s&include_entities=true&result_type=recent" % (user, number)))
+            pattern = re.compile('(\'media_url\':\s?u\')(http://[^\s]+)\'')
+            result = []
+            [ result.append(line[1]) for line in re.findall(pattern, str(answer)) if not line[1] in result ]
+            self.parent.conn.privmsg(self.channel, random.choice(result))
         except:
             return False
 
