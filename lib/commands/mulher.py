b672ab3c63e4283ff13538df0b978742b53ead54
Rogerio Goncalves <rogerlz@gmail.com>
2013-12-06 17:55:21 -0200

    rename !help to !ajuda and fix !mulher

--- lib/commands/mulher.py
+++ lib/commands/mulher.py
@@ -6,17 +6,14 @@ from lib.modules.database import Database
 class Mulher(Base_Command.Base_Command):
 
     def mulher(self):
-        if len(self.args) < 1:
-            self.parent.conn.privmsg(self.channel,'%s, deixa de ser burro e mulher alguém. ' % self.nick)
-        else:
-            db = Database()
-            self.mulher = None
-            try:
-                self.mulher = db.select('mulher', 'mulheres', 1)[0][0]
-            except:
-                return False
-            if self.mulher:
-                self.parent.conn.privmsg(self.channel, '%s, %s' % (self.args[0], self.mulher))
+        db = Database()
+        self.mulher = None
+        try:
+            self.mulher = db.select('mulheres', 'mulher', 1)[0][0]
+        except:
+            return False
+        if self.mulher:
+            self.parent.conn.privmsg(self.channel, self.mulher)
 
     def run(self):
         self.mulher()
